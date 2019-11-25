250114b8641b3a5e5c3d1a4ae0a151623070beb2
Brian Norman <briankn8@gmail.com>
2019-11-24 20:04:04 -0800

    Refactor (#35)

--- app/src/main/java/com/cobresun/factfulnewsandroid/ui/SettingsFragment.kt
+++ app/src/main/java/com/cobresun/factfulnewsandroid/ui/SettingsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.preference.CheckBoxPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SeekBarPreference
 import com.cobresun.factfulnewsandroid.R
 import com.cobresun.factfulnewsandroid.repositories.impl.SharedPrefsUserDataRepository
 
@@ -12,11 +11,10 @@ open class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
-        val readTimePreference: SeekBarPreference? = findPreference("read_time")
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        if(preference.key.startsWith("category_")){
+        if (preference.key.startsWith("category_")){
             val thisContext = context
             if (thisContext != null) {
                 val prefs = SharedPrefsUserDataRepository(thisContext)
