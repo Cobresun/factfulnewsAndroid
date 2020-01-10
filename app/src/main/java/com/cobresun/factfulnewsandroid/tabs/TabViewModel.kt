84eba4de7f7aa4c6e0055b93ae9ef00e8b596945
Brian Norman <b2norman@uwaterloo.ca>
2020-01-10 14:11:18 -0500

    Use sexy new liveData coroutine builder

--- app/src/main/java/com/cobresun/factfulnewsandroid/tabs/TabViewModel.kt
+++ app/src/main/java/com/cobresun/factfulnewsandroid/tabs/TabViewModel.kt
@@ -1,12 +1,13 @@
 package com.cobresun.factfulnewsandroid.tabs
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
 import com.cobresun.factfulnewsandroid.models.Article
 import com.cobresun.factfulnewsandroid.repositories.ArticlesRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 class TabViewModel(
@@ -15,21 +16,18 @@ class TabViewModel(
     private val articlesRepository: ArticlesRepository
 ) : ViewModel() {
 
-    private val _articles = MutableLiveData<List<Article>>().apply { value = emptyList() }
-    val articles: LiveData<List<Article>> = _articles
-
     private val handler = CoroutineExceptionHandler { _, exception ->
         Timber.e("Caught $exception")
     }
 
-    init {
-        viewModelScope.launch(Dispatchers.IO + handler) {
-            val articles = articlesRepository
-                .getArticles(tabCategory)
-                .filter { it.timeToRead <= readTime }
-            withContext(Dispatchers.Main) { _articles.value = articles }
-        }
+    private val _articles = liveData(Dispatchers.IO + handler) {
+        val articles = articlesRepository
+            .getArticles(tabCategory)
+            .filter { it.timeToRead <= readTime }
+        emit(articles)
     }
+    val articles: LiveData<List<Article>> = _articles
+
 }
 
 @Suppress("UNCHECKED_CAST")
